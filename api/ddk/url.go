b23aa10a427fcdb2071b7a46a88e59a97d6710f0
Syd Xu <prof.syd.xu@gmail.com>
2022-07-19 11:46:37 +0800

    feat(ddk): pdd.ddk.goods.promotion.url.generate支持获取微信ShortLink链接

--- api/ddk/url.go
+++ api/ddk/url.go
@@ -28,6 +28,10 @@ type PromURL struct {
 	SchemaURL string `json:"schema_url,omitempty" xml:"schema_url,omitempty"`
 	// TzSchemaURL 使用此推广链接，用户安装多多团长APP的情况下会唤起APP（需客户端支持schema跳转协议）
 	TzSchemaURL string `json:"tz_schema_url,omitempty" xml:"tz_shceme_url,omitempty"`
+	// WeixinCode 微信小程序码
+	WeixinCode string `json:"weixin_code,omitempty" xml:"weixin_code,omitempty"`
+	// WexinShortLink 微信ShortLink链接
+	WexinShortLink string `json:"weixin_short_link,omitempty" xml:"weixin_short_link,omitempty"`
 	// MultiURLLIst 双人团链接列表
 	MultiURLList *PromURL `json:"multi_url_list,omitempty" xml:"multi_url_list,omitempty"`
 	// SingleURLList 单人团链接列表
