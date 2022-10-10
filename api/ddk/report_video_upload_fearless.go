3cee2550b3b7d940a4e4c4415c6372f24dee1e20
Syd Xu <prof.syd.xu@gmail.com>
2022-10-10 17:46:10 +0800

    fix(ddk): 视频分片上传

--- api/ddk/report_video_upload_fearless.go
+++ api/ddk/report_video_upload_fearless.go
@@ -11,8 +11,9 @@ import (
 // 多多客信息流投放备案视频上传,上传视频大小有限制,单个文件超过20M需要走分片上传
 // 根据上传文件大小自动选择API接口
 func ReportVideoUploadFearless(clt *core.SDKClient, req *ReportVideoUploadRequest, size int64) (string, error) {
-	var chunkSize int64 = 20 * 1 << 20
-	if size <= chunkSize {
+	var maxSize int64 = 20 * 1 << 20
+	var chunkSize int64 = 5 * 1 << 20
+	if size <= maxSize {
 		return ReportVideoUpload(clt, req)
 	}
 	uploadSign, err := ReportVideoUploadPartInit(clt, "video/mp4")
@@ -21,7 +22,7 @@ func ReportVideoUploadFearless(clt *core.SDKClient, req *ReportVideoUploadReques
 	}
 	var (
 		fr      = bufio.NewReader(req.File)
-		b       = make([]byte, 0, chunkSize)
+		b       = make([]byte, chunkSize)
 		partNum int
 	)
 	for {
@@ -39,6 +40,7 @@ func ReportVideoUploadFearless(clt *core.SDKClient, req *ReportVideoUploadReques
 		if err != nil {
 			return "", err
 		}
+		partNum++
 	}
 	return ReportVideoUploadPartComplete(clt, uploadSign)
 }
